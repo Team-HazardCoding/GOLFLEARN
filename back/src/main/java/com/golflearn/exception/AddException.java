548201c3e3ebf0323579d8f217a636e2aacffddd
future <rmh7290@gmail.com>
2022-07-04 19:48:18 +0900

    Add packages and update gitignore

---
+++ back/src/main/java/com/golflearn/exception/AddException.java
@@ -0,0 +1,13 @@
+package com.golflearn.exception;
+
+public class AddException extends Exception {
+	public AddException() {
+		super();
+	}
+
+	public AddException(String message) {
+		super(message);
+	}
+
+	
+}
