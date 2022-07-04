fef2c7da71f5a1a0c3238462bda05254d7ac4798
cokesh <spdlqjlwjs@daum.net>
2022-07-04 23:11:19 +0900

    Delete : useless code

--- back/src/main/java/com/golflearn/control/ViewLessonServlet.java
+++ back/src/main/java/com/golflearn/control/ViewLessonServlet.java
@@ -15,7 +15,7 @@ public class ViewLessonServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 }
