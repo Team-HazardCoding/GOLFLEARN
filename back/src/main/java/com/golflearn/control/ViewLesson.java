d17e9c966b6ebbf6c30bf51ac821f0e91bbe971a
cokesh <spdlqjlwjs@daum.net>
2022-07-04 19:52:02 +0900

    fix viewLessonServlet

--- back/src/main/java/com/golflearn/control/ViewLesson.java
+++ back/src/main/java/com/golflearn/control/ViewLesson.java
@@ -14,18 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class ViewLesson extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public ViewLesson() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
