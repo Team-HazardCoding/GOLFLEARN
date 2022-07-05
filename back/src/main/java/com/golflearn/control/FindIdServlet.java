ae4ccdd210af33ab7469dbc0e8d6657f64258a18
yeon <choyeon960325@gmail.com>
2022-07-05 16:09:18 +0900

    Add codes on FindIdServlet

--- back/src/main/java/com/golflearn/control/FindIdServlet.java
+++ back/src/main/java/com/golflearn/control/FindIdServlet.java
@@ -1,20 +1,44 @@
 package com.golflearn.control;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class FindIdServlet
- */
+import com.golflearn.sql.MyConnection;
+
 @WebServlet("/findid")
 public class FindIdServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("user_id");
+		String name = request.getParameter("user_name");
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("finIdInfo");
+		
+		try {
+			con = MyConnection.getConnection();
+			String select
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
 
