f7a32ee276e245676ea5cc94bbd33bf545b35658
yeon <choyeon960325@gmail.com>
2022-07-06 10:20:30 +0900

    Modify code on MyConnection.java file

--- back/src/main/java/com/golflearn/sql/MyConnection.java
+++ back/src/main/java/com/golflearn/sql/MyConnection.java
@@ -25,11 +25,12 @@ public class MyConnection {
 		//3. DB와 연결
 		Connection con = null ;
 //		String url = "jdbc:oracle:thin:@localhost:1124:xe"; 
-		String url = "jdbc:oracle:thin:@jongwoo_high?TNS_ADMIN=/C:/Golflearn_lib/전자지갑/Wallet_Jongwoo.zip";
+		String url = "jdbc:oracle:thin:@jongwoo_high?TNS_ADMIN=C://Golflearn_lib/전자지갑";
 		String user = "admin";
 		String password = "Hazard11241124";
-
+		
 		con = DriverManager.getConnection(url, user, password);
+		System.out.println("db 연결성공");
 		return con; 
 	}
 	public static void close(ResultSet rs, Statement stmt, Connection con) {
