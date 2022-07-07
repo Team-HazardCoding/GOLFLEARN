1f19d34516cb091bf9e8b20bcb464ddac11c0246
Kevin Jo <jokw9250@naver.com>
2022-07-07 20:19:31 +0900

    Add calling selectByLsnNo method

--- back/src/main/java/com/golflearn/control/ViewStudentManageServlet.java
+++ back/src/main/java/com/golflearn/control/ViewStudentManageServlet.java
@@ -1,25 +1,39 @@
 package com.golflearn.control;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class ViewStudentManageServlet
- */
+import com.golflearn.exception.FindException;
+import com.golflearn.repository.LessonHistoryRepository;
+
 @WebServlet("/viewstudentmanage")
 public class ViewStudentManageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
 		response.setContentType("text/plain;charset = utf-8");
+//		int selectedLsnNo = Integer.parseInt(request.getParameter("lsn_no"));
 		
+		LessonHistoryRepository lhrepo = new LessonHistoryRepository();
+		
+		try {
+			System.out.println(lhrepo.selectByLsnNo(1));
+		} catch (FindException e) {
+			e.printStackTrace();
+		}
+		
+//		try {
+//			lhrepo.selectByLsnNo(selectedLsnNo);
+//		} catch (FindException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
