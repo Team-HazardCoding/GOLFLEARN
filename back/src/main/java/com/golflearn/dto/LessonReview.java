658475c483f10573be5b12c49011b6e02d5cb003
Kevin Jo <jokw9250@naver.com>
2022-07-07 09:49:02 +0900

    ADD : getter, setter methods in LessonReview.java

--- back/src/main/java/com/golflearn/dto/LessonReview.java
+++ back/src/main/java/com/golflearn/dto/LessonReview.java
@@ -8,4 +8,46 @@ public class LessonReview {
 	private String review;
 	private Date reviewDt;
 	private Date reviewEditDt;
+	//세터 게터 메서드 
+	//레슨내역번호
+	public int getLsnLineNo() {
+		return this.lsnLineNo;
+	}
+	public void setLsnLineNo(int lsnLineNo) {
+		this.lsnLineNo = lsnLineNo;
+	}
+	
+	//별점 
+	public int getMyStarScore() {
+		return this.myStarScore;
+	}
+	public void setMyStarScore(int myStarScore) {
+		this.myStarScore = myStarScore;
+	}
+	
+	//후기 
+	public String getReview() {
+		return this.review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	
+	//후기일자
+	public Date getReviewDt() {
+		return this.reviewDt;
+	}
+	public void setReviewDt(Date reviewDt) {
+		this.reviewDt = reviewDt;
+	}
+	
+	//후기수정일자
+	public Date getReviewEditDt() {
+		return this.reviewEditDt;
+	}
+	public void setReviewEditDt(Date reviewEditDt) {
+		this.reviewEditDt = reviewEditDt;
+	}
+	
+
 }
