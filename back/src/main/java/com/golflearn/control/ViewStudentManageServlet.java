b746362399221d98a894931c955ef91f86da00bc
Kevin Jo <jokw9250@naver.com>
2022-07-07 09:47:47 +0900

    CREATE : ViewStudentManageServlet

--- back/src/main/java/com/golflearn/control/ViewStudentManageServlet.java
+++ back/src/main/java/com/golflearn/control/ViewStudentManageServlet.java
@@ -18,8 +18,8 @@ public class ViewStudentManageServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/plain;charset = utf-8");
+		
 	}
 
 }
