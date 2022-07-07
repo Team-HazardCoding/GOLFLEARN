1c7c2f54e787d1feacec1c98e96568f93e04be5f
Kevin Jo <jokw9250@naver.com>
2022-07-07 09:50:17 +0900

    ADD : selectByLsnLineNO method. but it needs to be refactored asap

--- back/src/main/java/com/golflearn/repository/LessonLineRepository.java
+++ back/src/main/java/com/golflearn/repository/LessonLineRepository.java
@@ -25,25 +25,27 @@ public class LessonLineRepository {
 	 */
 
 	/**
-	 * lsn_line_no 정렬순대로 나타내게 하기 
+	 * 마이페이지 로딩되었을 시 나타낼 정보 
 	 * 93saewoo는 입력된 아이디로 바꾸기 
+	 * 유저아이디와 같은 레슨내역 보여주기 
 	 * @param lessonLineNo
 	 * @return
 	 */
-	public List<LessonLine> selectByLessonLineNo(String lessonLineNo){
+	public List<LessonLine> selectByLsnLineNo(String lessonLineNo){
 		
+		//user_id 받아오기 
 		List<LessonLine> lines = new ArrayList<>();
 		
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String test = lessonLineNo;
+		String test = lessonLineNo; //테스트로 임시로 만듦 나중에 지우기
 
 		//lesson_line 테이블에서 lesson_line가 ?인 모든 컬럼을 가져오기  -> viewmypage
 		String selectLessonLineNo = 
 				"SELECT *\r\n"
 				+ "FROM lesson_line \r\n"
-				+ "WHERE lsn_line_no = ?";
+				+ "WHERE User = ?";
 		try {
 			con = MyConnection.getConnection();
 			pstmt = con.prepareStatement(selectLessonLineNo);
@@ -59,11 +61,11 @@ public class LessonLineRepository {
 				Date lsnAplyDt = rs.getDate("lsn_aply_dt");
 				Date lsnExpDt = rs.getDate("lsn_exp_dt");
 				int stdtLsnStatus = rs.getInt("stdt_lsn_status");
-				System.out.println("레슨내역번호는 : " + lsnLineNo 
-								   + "유저아이디는 : " + userId
-							       + "레슨번호는 : " + lsnNo
-							       + "레슨신청일은 : " + lsnAplyDt
-							       + "레슨만료일은 : " + lsnExpDt);	
+//				System.out.println("레슨내역번호는 : " + lsnLineNo 
+//								   + "유저아이디는 : " + userId
+//							       + "레슨번호는 : " + lsnNo
+//							       + "레슨신청일은 : " + lsnAplyDt
+//							       + "레슨만료일은 : " + lsnExpDt);	
 				
 				if(stdtLsnStatus == 0) {
 					System.out.println("수강생입니다");
@@ -81,6 +83,17 @@ public class LessonLineRepository {
 				l.setLsnExpDt(lsnExpDt); 
 				l.setStdtLsnStatus(stdtLsnStatus); 
 				lines.add(l);
+				System.out.println(lines.toString());
+				System.out.println("tostring경계");
+				System.out.println(lines.get(0));
+				//쌤예시 
+//				Map<String,Object> map1 = new HashMap<>();
+//				map1.put("prod_no", prod_no);
+//				map1.put("prod_name", prod_name);
+//				map1.put("prod_price", prod_price);
+//				sample.add(map1); //List타입의 sample변수에 map을 저장해보았음 
+				
+				
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -90,7 +103,7 @@ public class LessonLineRepository {
 		return lines;
 //		return null; //임시 리턴 
 
-
+		
 	}
 
 }
