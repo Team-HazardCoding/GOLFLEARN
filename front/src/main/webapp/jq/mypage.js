1e6ca3f08da4e426bedf2a48ca1f7785272bca1c
Kevin Jo <jokw9250@naver.com>
2022-07-10 14:01:06 +0900

    Add frontend html, js realted to mypage, mypropage'

--- front/src/main/webapp/jq/mypage.js
+++ front/src/main/webapp/jq/mypage.js
@@ -1,6 +1,16 @@
-$(function)(){
-    let $cancelObj = $('input[type=button]');
+$(function(){
+
+    //레슨이 눌렸을 때 레슨상세페이지로 연결 
+    //레슨 내역에서 아무거나 눌러도 연결되게 -> 만약 img같은것에도 
+    //일일이 달아줘야하는 경우 설정하기 
+    let $viewlsnObj = $('div.lsnlist');
+    $viewlsnObj.click(function(){
+        // location.href("/front/html/종우가정한링크")
+        $(location).attr('href', '/front/html/mypropage.html');
+    });
     
+    //수강취소버튼 눌렀을 시 alert
+    let $cancelObj = $('input[type=button]');
 
 
 
@@ -13,4 +23,5 @@ $(function)(){
 
 
 
-}
+
+})
